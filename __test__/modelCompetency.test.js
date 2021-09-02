c03f3a54930792ebf8c984fedaebbac34a90e190
AlfalahM273 <MuhammadAlfalah.Madukubah@mitrais.com>
2021-09-02 17:42:58 +0800

    feat: add unit test on competency model

--- __test__/modelCompetency.test.js
+++ __test__/modelCompetency.test.js
@@ -1,6 +1,11 @@
+const fsPromises = require("fs").promises;
+const fs = require('fs');
+
 const competencyModel = require("../model/competency");
 
-describe("Person Get Data", () => {
+
+
+describe("Get Data", () => {
   test("get all", () => {
     return competencyModel.get().then((result) => {
       expect(Array.isArray(result)).toBeTruthy();
@@ -9,75 +14,127 @@ describe("Person Get Data", () => {
 });
 
 describe("Insert Data", () => {
-  test("all req field filled", () => {
+  beforeEach(() => {
+    const DB_PATH = "./db/competency.json";
+    const DB_SEQ_PATH = "./db/competency_seq.json";
+    let dataSeqJson = { current_id: 0, next_id: 1 }
+    fs.writeFileSync(DB_SEQ_PATH, JSON.stringify(dataSeqJson));
+    fs.writeFileSync(DB_PATH, JSON.stringify([]));
+  });
+  test("all req field filled", async () => {
     let dataObj = {
       skill: "Node",
       level: "Entry",
       selfClaim: "selfClaim",
     };
-    return competencyModel.create(dataObj).then((result) => {
-      expect(result).toBeTruthy();
+    await expect(competencyModel.create(dataObj)).resolves.toBeDefined();
+    await competencyModel.create(dataObj).then((result) => {
+      expect(result.skill).toBe("Node");
     });
   });
 
-  test("one of req field missing", () => {
+  test("one of req field missing", async () => {
     let dataObj = {
       skill: "Node",
       level: "Entry",
     };
-    return competencyModel.create(dataObj).then((result) => {
-      expect(result).toBeFalsy();
+    await expect(competencyModel.create(dataObj)).rejects.toBeDefined();
+    await competencyModel.create(dataObj).catch((err) => {
+      expect(err).toBe("All field required");
     });
   });
 
-  test("one of req field empty", () => {
+  test("one of req field empty", async () => {
     let dataObj = {
       skill: "Node",
       level: "",
       selfClaim: "selfClaim",
     };
-    return competencyModel.create(dataObj).then((result) => {
-      expect(result).toBeFalsy();
+    await expect(competencyModel.create(dataObj)).rejects.toBeDefined();
+    await competencyModel.create(dataObj).catch((err) => {
+      expect(err).toBe("All field required");
     });
   });
 });
 
 describe("Update Data", () => {
-  test("field is filled", () => {
+  beforeEach(() => {
+    const DB_PATH = "./db/competency.json";
+    const DB_SEQ_PATH = "./db/competency_seq.json";
+    let dataSeqJson = { current_id: 1, next_id: 2 }
+    fs.writeFileSync(DB_SEQ_PATH, JSON.stringify(dataSeqJson));
+    fs.writeFileSync(DB_PATH, JSON.stringify([
+      {
+        id: 1,
+        skill: "Node",
+        level: "Level",
+        selfClaim: "selfClaim",
+      }
+    ]));
+  });
+  test("field is filled", async () => {
     let dataObj = {
-      skill: "Node",
-      level: "",
-      selfClaim: "selfClaim",
+      skill: "Node Update",
+      level: "Level Update",
+      selfClaim: "selfClaim Update",
     };
     let id = 1;
-    return competencyModel.update(id, dataObj).then((result) => {
-      expect(Array.isArray(result)).toBeTruthy();
+    await expect(competencyModel.update(id, dataObj)).resolves.toBeDefined();
+    await competencyModel.update(id, dataObj).then((result) => {
+      expect(result).toBeDefined();
+      expect(result.skill).toEqual("Node Update");
     });
   });
-  test("id not found", () => {
+
+  test("id not found", async () => {
+    let dataObj = {
+      skill: "Node",
+      level: "Level Update",
+      selfClaim: "selfClaim",
+    };
+    let id = -1;
+    await expect(competencyModel.update(id, dataObj)).resolves.toBeNull();
+  });
+
+  test("one of req field missing", async () => {
     let dataObj = {
       skill: "Node",
       level: "",
       selfClaim: "selfClaim",
     };
     let id = -1;
-    return competencyModel.update(id, dataObj).then((result) => {
-      expect(Array.isArray(result)).toBeFalsy();
+    await expect(competencyModel.update(id, dataObj)).rejects.toBeDefined();
+    await competencyModel.update(id, dataObj).catch((err) => {
+      expect(err).toBe("All field required");
     });
   });
 });
 
 describe("Delete Data", () => {
-  test("id found", () => {
-    let id = -1;
-    return competencyModel.unlink(id).then((result) => {
-      expect(Array.isArray(result)).toBeTruthy();
+  beforeEach(() => {
+    const DB_PATH = "./db/competency.json";
+    const DB_SEQ_PATH = "./db/competency_seq.json";
+    let dataSeqJson = { current_id: 1, next_id: 2 }
+    fs.writeFileSync(DB_SEQ_PATH, JSON.stringify(dataSeqJson));
+    fs.writeFileSync(DB_PATH, JSON.stringify([
+      {
+        id: 1,
+        skill: "Node",
+        level: "Level",
+        selfClaim: "selfClaim",
+      }
+    ]));
+  });
+  test("id found", async () => {
+    let id = 1;
+    await competencyModel.unlink(id).then((result) => {
+      expect(result).toBeTruthy();
     });
   });
-  test("id not found", () => {
+  test("id not found", async () => {
     let id = -1;
-    return competencyModel.unlink(id).then((result) => {
-      expect(Array.isArray(result)).toBeFalsy();
+    await competencyModel.unlink(id).then((result) => {
+      expect(result).toBeNull();
     });
   });
 });
--- model/competency.js
+++ model/competency.js
@@ -1,6 +1,8 @@
 const fsPromises = require("fs").promises;
+const fs = require('fs');
 
 const DB_PATH = "./db/competency.json";
+const DB_SEQ_PATH = "./db/competency_seq.json";
 
 const get = () => {
   return fsPromises.readFile(DB_PATH).then((result) => {
@@ -9,10 +11,16 @@ const get = () => {
   });
 };
 
-const create = (dataObj) => {
+const create = async (dataObj) => {
   if (!dataObj.skill || !dataObj.level || !dataObj.selfClaim) {
-    return Promise.resolve(false);
+    return Promise.reject("All field required");
   }
+  let dataSeq = fs.readFileSync(DB_SEQ_PATH);
+  var dataSeqJson = JSON.parse(dataSeq);
+  dataObj.id = dataSeqJson.next_id;
+  dataSeqJson.current_id = dataSeqJson.next_id;
+  dataSeqJson.next_id += 1;
+  fs.writeFileSync(DB_SEQ_PATH, JSON.stringify(dataSeqJson));
 
   return fsPromises
     .readFile(DB_PATH)
@@ -23,20 +31,67 @@ const create = (dataObj) => {
       return fsPromises
         .writeFile(DB_PATH, JSON.stringify(resultJson))
         .then(() => {
-          return personJson;
+          return Promise.resolve(dataObj);
         })
         .catch((err) => {
-          return err;
+          return Promise.reject(err);
         });
     })
     .catch((err) => {
-      throw err;
+      return Promise.reject(err);
     });
 };
 
-const update = (id, newData) => {};
+const update = (id, newData) => {
+  if (!newData.skill || !newData.level || !newData.selfClaim) {
+    return Promise.reject("All field required");
+  }
+  return fsPromises.readFile(DB_PATH)
+    .then((result) => {
+      resultJson = JSON.parse(result);
+      let filteredData = resultJson.filter(e => e.id == id);
+      if (filteredData.length == 1) {
+        filteredData = { id: filteredData[0].id, ...newData };
+        let restData = resultJson.filter(e => e.id != id);
+        restData.push(filteredData);
+        return fsPromises.writeFile(DB_PATH, JSON.stringify(restData))
+          .then(() => {
+            return Promise.resolve(filteredData);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      } else {
+        return Promise.resolve(null);
+      }
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
 
-const unlink = (id) => {};
+const unlink = (id) => {
+  return fsPromises.readFile(DB_PATH)
+    .then((result) => {
+      resultJson = JSON.parse(result);
+      let filteredData = resultJson.filter(e => e.id == id);
+      if (filteredData.length == 1) {
+        let restData = resultJson.filter(e => e.id != id);
+        return fsPromises.writeFile(DB_PATH, JSON.stringify(restData))
+          .then(() => {
+            return Promise.resolve(filteredData);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      } else {
+        return Promise.resolve(null);
+      }
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
 
 module.exports = {
   get,
